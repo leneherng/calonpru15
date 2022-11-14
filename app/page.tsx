37c11a75cf095cfd9c97552ad23f84aed1ee3acb
Len <leneherng@gmail.com>
2022-11-15 02:01:47 +0800

    Deploy

--- app/page.tsx
+++ app/page.tsx
@@ -29,35 +29,6 @@ export default function Page() {
           <Candidates area={candidates[code]} />
         </div>
         </div>
-
-
-        {/* <div className="col-start-1">
-        <div className="text-xl font-medium text-gray-900">What is this?</div>
-        <div className="space-y-4">
-          <p className="space-y-2 text-sm text-gray-900">
-            This is a cartogram. It is an alternative method of displaying a
-            map.
-          </p>
-          <div className="text-md font-medium text-gray-900">
-            Why not use a map?
-          </div>
-          <p className="space-y-2 text-sm text-gray-900">
-            A couple of reasons:
-          </p>
-          <ul className="list-disc space-y-2 pl-4 text-sm text-gray-900">
-            <li>
-              The sizes of each state are drastically different when compared to
-              each other. This results in a map where areas such as the Wilayah
-              Persekutuan, become too tiny relative to other states.
-            </li>
-            <li>
-              Other states however (looking at you Sarawak), have parliamentary
-              seats that are drastically different in sizes, causing the
-              mismatch relative to other seats.
-            </li>
-          </ul>
-        </div>
-      </div> */}
       </div>
     </div>
   );
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 # Calon PRU15
+This is a cartogram. It is an alternative method of displaying a map.
+
+## Why not use a map?
+A couple of reasons:
+
+- The sizes of each state are drastically different when compared to
+each other. This results in a map where areas such as the Wilayah
+Persekutuan, become too tiny relative to other states.
+
+- Other states however (looking at you Sarawak), have parliamentary
+seats that are drastically different in sizes, causing the
+mismatch relative to other seats.
