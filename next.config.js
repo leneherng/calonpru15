391338e3cc4363a05c772d159902f454b8257453
Len <leneherng@gmail.com>
2022-11-15 01:07:29 +0800

    Changed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/calonpru15',
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   swcMinify: true,
   experimental: {
